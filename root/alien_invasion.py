749ae77aac760fbc6a0a0c6ba3234c47869a5e2c
Mohyio <18172755902@163.com>
2022-01-24 10:33:46 +0800

    2. set the background color

--- root/alien_invasion.py
+++ root/alien_invasion.py
@@ -7,14 +7,16 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))  # 实参(1200, 800) 是一个元组，因而需要两个括号
     pygame.display.set_caption("Alien Invasion")
+    bg_color = (230, 230, 230)  # 设置背景色
+    
     # 游戏主循环
     while True:
         # 监视 键鼠 事件
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        # 最近绘制屏幕可见
-        pygame.display.flip()
+        screen.fill(bg_color)   # 每次循环时都重绘屏幕
+        pygame.display.flip()           # 最近绘制屏幕可见
 
 
 run_game()
