a6bf5f2c8192ea045fa5b99f8f8ec0d68d7b352c
Mohyio <18172755902@163.com>
2022-01-30 10:26:01 +0800

    6。 clear all of change before

--- root/alien_invasion.py
+++ root/alien_invasion.py
@@ -1,4 +1,4 @@
-# import sys
+import sys
 import pygame
 
 from settings import Settings
@@ -10,15 +10,18 @@ def run_game():
     pygame.init()
     pygame.display.set_caption("Alien_Invasion_by_Mohyio")
     ai_st = Settings()  # 要使用,应该先赋予其类 (类赋予)
-    screen = pygame.display.set_mode((ai_st.screen_width, ai_st.screen_height))
+    screen = pygame.display.set_mode((ai_st.screen_width, ai_st.screen_height))  # 分配好长宽高给 screen 变量
     ship = Ship(screen)
     bg_color = ai_st.bg_color
     while True:
         gf.check_events()
-        gf.update_screen(ai_st , screen=screen, ship=ship)
-        # screen.fill(bg_color)
-        # ship.blitme()
-        # pygame.display.flip()
+        gf.update_screen(ai_settings, screen, ship)
+        """for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()"""
+        screen.fill(bg_color)
+        ship.blitme()   # 指定位置绘制飞船
+        pygame.display.flip()
 
 
 run_game()
--- root/ship.py
+++ root/ship.py
@@ -4,13 +4,12 @@ import pygame
 class Ship():
     def __init__(self, screen):
         self.screen = screen
-        self.image = pygame.image.load('photo/ship.png') # photo/ship.png # /Users/chenzhiyu/vscodeProjects/Alien_Invasion/photo/ship.png
-        self.rect = self.image.get_rect()   # 使得图片为矩形， 得到 surface属性
-        self.screen_rect = screen.get_rect()    # 将矩形存储到 该方法中
+        self.image = pygame.image.load('../photo/ship.png')
+        self.rect = self.image.get_rect()  # 使得图片为矩形， 得到 surface属性
+        self.screen_rect = screen.get_rect()  # 将矩形存储到 该方法中
         # 与下两句组合成为下方中间； 起始(0,0)为左上角
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom  # 可能是固定用法，self.rect.[*] 与 self.screen_rect.[*] 要保持一致 ： bottom/center/top/bottom / centerx/centery
 
-
-    def blitme(self):
+    def blitme(self):  # 指定位置绘制飞船
         self.screen.blit(self.image, self.rect)
